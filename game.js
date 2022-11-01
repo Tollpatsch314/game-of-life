86b1edc8607be1658fd9dc7d88518acb5ccf49e4
Tollpatsch314 <tollpatsch3141@web.de>
2022-11-01 15:18:14 +0100

    42

--- game.js
+++ game.js
@@ -8,46 +8,62 @@ function ASSERT(condition, msg) {
 
 function initGame(SIZE)
 {                                           // setzt die Spielfeldgröße und erstellt das Spielfeldarray
-	ASSERT(typeof SIZE === "number")
+	ASSERT(typeof SIZE === "number");
 	game_field_size = SIZE;
 	game_field = genGameField(SIZE);
+
+	gameIteration();
 }
 
 function genGameField(SIZE)	
 {											// erstellt das Spielfeldarray
 	
-	ASSERT(typeof SIZE === "number")
-	let field = new Int8Array(SIZE);        // Integer statt Booleans (lassen sich beim Zählen besser addieren)
-	field.forEach( x => {
-			x = new Int8Array(SIZE);
-			x.forEach(y => { y = 0 });
-	});
+	ASSERT(typeof SIZE === "number");
+	let field = new Array(SIZE).fill(new Array(SIZE).fill(0));        // Integer statt Booleans (lassen sich beim Zählen besser addieren)
+	console.log(field);
+
 	return field;
 }
 
-function getNeighborsCount(index, border = [0, 0])
+function getNeighborsCount(index, border = [[-1, 1], [-1, 1]])
 {
-	const [x, y] = index;
-	const [x_border, y_border] = border;
+	let x = index[0], y = index[1];
+	//var [[l, r], [t, b]] = border; 	// [[left, rigth], [top, bottom]]
+	let l = border[0][0], r = border[0][1], t = border[1][0], b = border[1][1]; 	// [[left, rigth], [top, bottom]]
+	console.log (x + "|" + y + "|" + l + "|" + r + "|" + t + "|" + b);
 
-	return  game_field[x + 1][y + 1] + game_field[x + 1][y] + game_field[x + 1][y - 1] + 
-			game_field[  x  ][y + 1] + /*     Index      */   game_field[  x  ][y - 1] + 
-			game_field[x - 1][y + 1] + game_field[x - 1][y] + game_field[x - 1][y - 1];
+	return (
+		game_field[x + l][y + t] + game_field[  x  ][y + t] + game_field[x + r][y + t] + 
+		game_field[x + l][  y  ] + /*       Index        */   game_field[x + r][  y  ] + 
+		game_field[x + l][y + b] + game_field[  x  ][y + b] + game_field[x + r][y + b]
+	);
 }
 
-function willDie(index)
-{
-	let neighbors_count = getNeighborsCount(index);
+function willDie(index, border) {
+	let neighbors_count = getNeighborsCount(index, border);
 	return neighbors_count >= 2 && neighbors_count <= 3;
+}
+
+function getNewValue(index, border) {
+	return parseInt(willDie(index, border));
 }
 
 function gameIteration() {
 	var new_game_field = genGameField(game_field_size);
 
-	for (var x = 1; x < game_field.length - 1; x++) {
-		getNeighborsCount([0])
+	for (var x = 0; x < game_field.length; x++) {
+
+		var border = [[-1, 1], [-1, 1]];
+		if(x == 0) border = [[game_field_size - 1, 1], [- 1, 1]];
+		else if (x == game_field.length - 1) border = [[- 1, - game_field_size - 1], [- 1, 1]]
+		else {
+			new_game_field[x][0] = getNewValue([x, 0], [[-1, 1], [game_field_size - 1, 1]]);
+			new_game_field[x][game_field_size - 1] = getNewValue([x, 0], [[- 1, 1], [- 1, - game_field_size + 1]]);
+			console.log("fsaf");
+		}
+
 		for (var y = 1; y < game_field[x].length - 1; y++) {
-			new_game_field[x][y] = parseInt(willDie([x, y]));
+			new_game_field[x][y] = getNewValue([x, y], border);
 		}
 
 	}
