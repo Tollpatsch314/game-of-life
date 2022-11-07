4d618972f993b91ab238a6aa2ae2e67dbc97af8e
Tollpatsch314 <tollpatsch3141@web.de>
2022-11-07 13:26:15 +0100

    random function

--- ts/functionality.ts
+++ ts/functionality.ts
@@ -20,12 +20,12 @@ function getFieldPos(mousePos: {x: number, y: number}) : {x: number, y: number} 
 	};
 }
 
-var mousedown: boolean, mouseIsSetting: boolean;
+var mousedown: boolean, mouseIsSetting: boolean, veryFirstInit: boolean = true;
 function loadPage() : void {	// Funktion lädt die Seite und den Canvas, sowie wichtige Variablen
 	canvas = document.getElementById("game-field") as HTMLCanvasElement;
 	ctx = canvas.getContext("2d");
 
-	function calcCtxSize() {
+	function calcCtxSize() {	// Berechnung des Context vom Canvas
 		let min = Math.min(window.innerHeight, window.innerWidth);
 		ctx.canvas.width = min / 10 * 9;
 		ctx.canvas.height = min / 10 * 9;
@@ -45,38 +45,45 @@ function loadPage() : void {	// Funktion lädt die Seite und den Canvas, sowie w
 	// Pause => Start
 	pauseGame();
 	
-	function drawEvent(evt: MouseEvent) : void {
-		var pos = getFieldPos(getMousePos(canvas, evt));
-		gameField.setCell(pos.x, pos.y, mouseIsSetting);
-		gameField.draw();
+	// event-Funktionen
+	if(veryFirstInit) {
+
+		function drawEvent(evt: MouseEvent) : void {
+			var pos = getFieldPos(getMousePos(canvas, evt));
+			gameField.setCell(pos.x, pos.y, mouseIsSetting);
+			gameField.draw();
+		}
+
+		canvas.addEventListener("mousedown", (evt) => {
+			mousedown = true;
+			var pos = getFieldPos(getMousePos(canvas, evt));
+			mouseIsSetting = !gameField.getCell(pos.x, pos.y);
+		});
+
+		canvas.addEventListener("mouseup", (evt) => {
+			drawEvent(evt);
+			mousedown = false;
+		});
+
+		canvas.addEventListener("mousemove", (evt) => {
+			if(!mousedown) return;
+			drawEvent(evt);
+		});
+
+		canvas.addEventListener("mouseout", () => {
+			mousedown = false;
+		})
+
+		canvas.addEventListener("click", drawEvent);
+
+		window.addEventListener("resize", () => {
+			calcCtxSize();
+			gameField.draw();
+		});
+
+		veryFirstInit = false;
+		f_pentomino();					// <======== Default Figure
 	}
-
-	canvas.addEventListener("mousedown", (evt) => {
-		mousedown = true;
-		var pos = getFieldPos(getMousePos(canvas, evt));
-		mouseIsSetting = !gameField.getCell(pos.x, pos.y);
-	});
-
-	canvas.addEventListener("mouseup", (evt) => {
-		drawEvent(evt);
-		mousedown = false;
-	});
-
-	canvas.addEventListener("mousemove", (evt) => {
-		if(!mousedown) return;
-		drawEvent(evt);
-	});
-
-	canvas.addEventListener("mouseout", () => {
-		mousedown = false;
-	})
-
-	canvas.addEventListener("click", drawEvent);
-
-	window.addEventListener("resize", () => {
-		calcCtxSize();
-		gameField.draw();
-	});
 }
 
 function rand(min: number, max: number) : number {
@@ -86,6 +93,14 @@ function rand(min: number, max: number) : number {
 function randInit(percentageLivingCells: number) : void {
 	let absoluteCount = Math.round((percentageLivingCells / 100) * gameField.rows * gameField.cols);
 	absoluteCount -= gameField.getLivingCellCount();
+
+	let add_val: boolean;
+	if(absoluteCount == 0) return;
+	else if(absoluteCount > 0) add_val = true;
+	else {
+		add_val = false;
+		absoluteCount *= -1;
+	}
 
 	function genX() : number { return rand(0, gameField.cols); }
 	function genY() : number { return rand(0, gameField.rows); }
@@ -99,8 +114,8 @@ function randInit(percentageLivingCells: number) : void {
 			let x: number = rand(-2, 2) + x_spot, y: number = rand(-2, 2) + y_spot;
 			x = (x + gameField.cols) % gameField.cols, y = (y + gameField.rows) % gameField.rows;
 
-			if(!gameField.getCell(x, y))  {
-				gameField.setCell(x, y, true);
+			if(gameField.getCell(x, y) ? !add_val : add_val)  {					// leider gibt es kein XOR in JS ):
+				gameField.setCell(x, y, add_val);
 				n++;
 			}
 
@@ -158,17 +173,22 @@ function changeInterval() : void {
 function drawField(field: GField) : void {
 	let xCan, yCan, width;
 
+	// Generations-Label setzten
 	let lblGen = document.getElementById("lbl-generation") as HTMLLabelElement;
 	lblGen.textContent = "Generation: " + game.getGeneration();
+
+	let count = 0;
 
 	ctx.clearRect(0, 0, canvas.width, canvas.height);
 	for (let x = 0; x < gameField.cols; x++) {
 		for (let y = 0; y < gameField.rows; y++) {
-				xCan = x * ctx.canvas.height / gameField.cols;
-				yCan = y * ctx.canvas.height / gameField.cols;
-				width = ctx.canvas.height / gameField.cols;
+			xCan = x * ctx.canvas.height / gameField.cols;
+			yCan = y * ctx.canvas.height / gameField.cols;
+			width = ctx.canvas.height / gameField.cols;
+
 			if (field[x][y] != 0) {
 				ctx.fillRect(xCan, yCan, width, width);
+				count++;
 			}
 			else {
 				ctx.fillStyle = "rgba(0, 0, 0, 0)";	
@@ -178,12 +198,17 @@ function drawField(field: GField) : void {
 			}
 		}
 	}
+
+	// Anteil an lebendigen Zellen berechnen
+	let percentageLivingCells = 100 * count / gameField.cols * gameField.rows;
+	// TODO: setzen des Sliders
 }
 
 function f_pentomino() : void {
-    gameField.field[25][25] = 1;
-    gameField.field[25][26] = 1;
-    gameField.field[25][27] = 1;
-    gameField.field[26][25] = 1;
-    gameField.field[24][26] = 1;
+	gameField.setCell(25, 25, true);
+	gameField.setCell(25, 26, true);
+	gameField.setCell(25, 27, true);
+	gameField.setCell(26, 25, true);
+	gameField.setCell(24, 26, true);
+	gameField.draw();
 }
